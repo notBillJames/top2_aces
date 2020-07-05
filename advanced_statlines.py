2afb4220a40abcb2e9d55c54258692cc9958aa21
Chuck Lyman <67621470+notBillJames@users.noreply.github.com>
2020-07-05 16:21:55 -0700

    format_df doesn't return a new df anymore, added OPS and SLG ColFunctions

--- advanced_statlines.py
+++ advanced_statlines.py
@@ -9,11 +9,15 @@ ColFunction = namedtuple('StatFunction', 'name func')
 PA_lambda = lambda row: row.AB + row.BB + row.HBP + row.SH + row.SF
 OBP_lambda = lambda row: (row.H + row.BB + row.HBP) / (row.PA) if row.PA > 0 else np.NaN
 AVG_lambda = lambda row: row.H / row.AB if row.AB > 0 else np.NaN
+SLG_lambda = lambda row: (row.H + row['2B'] + 2 * row['3B'] + 3 * row.HR) / row.AB if row.AB > 0 else np.NaN
+OPS_lambda = lambda row: row.SLG + row.OBP
 
 stat_functions = [
     ColFunction(name='PA', func=PA_lambda),
     ColFunction(name='OBP', func=OBP_lambda),
-    ColFunction(name='AVG', func=AVG_lambda)
+    ColFunction(name='AVG', func=AVG_lambda),
+    ColFunction(name='SLG', func=SLG_lambda),
+    ColFunction(name='OPS', func=OPS_lambda)
 ]
 
 def format_df(df):
